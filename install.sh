26e82461fea0c127751476bc186f40af822f5423
JEA666 <janerik.andersen@gmail.com>
2018-02-01 01:35:13 +0100

    Lots of bug fixses..

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 # https://google.github.io/styleguide/shell.xml
 
-# Text color variables
-readonly RED='\033[0;31m'
-readonly YEL='\033[0;33m'
-readonly GRE='\033[0;32m'
-readonly NC='\033[0m'
-
-# Get name of script #
-readonly SERVICENAME=$(basename $0)
-
 # test for ansible and install, if false
 if ( which ansible > /dev/null );then
     echo "Ansible existerer"
@@ -19,5 +10,5 @@ else
 fi
 
 # Install software and setup environment
-printf "\n"${YEL}"Installing software and customize your ubuntu innstall" "${NC}"\n
-ansible-playbook ubuntudesktop.yml --ask-become-pass -vvvv -C
+echo "Installing software and customize your ubuntu innstall"
+ansible-playbook ubuntudesktop.yml --ask-become-pass -vvvv
