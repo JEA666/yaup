c4783e6b70d7193b66649aa11546f849e5c2dd70
jea <janerik.andersen@gmail.com>
2019-11-27 22:15:28 +0100

    try to make at test for nye playbooks

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,5 +11,10 @@ else
     apt install ansible -y
 fi
 
+cat $HOME/git/pri/yaup/inventory/local/host_vars/priHost <<EOF
+priGitUserName: GitUser
+priUserMail: GitUser@example.com
+priUserName: testUser
+
 echo "To run Ansible  type in this command"
 echo "ansible-playbook ubuntudesktop.yml --ask-become-pass" 
